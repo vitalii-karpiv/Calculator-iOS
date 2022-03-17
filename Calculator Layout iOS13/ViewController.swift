8acfec8d507aacfce6ecf149735eb1f8eebb4e6f
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-03-17 21:39:01 +0200

    add functionality for cleaning label, persentage sign, -/+ sign

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTyping = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert text label into double")
+            }
+            return number
+        }
+        set {
+                displayLabel.text! = String(newValue)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTyping = true
+        
+        if let culcMethod = sender.currentTitle {
+            switch culcMethod {
+            case "+/-":
+                displayValue *= -1
+                break
+            case "%":
+                displayValue /= 100
+                break
+            default:
+                displayLabel.text = "0"
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numTitle = sender.currentTitle {
+            if isFinishedTyping {
+                displayLabel.text = numTitle
+                isFinishedTyping = false
+            } else {
+                if numTitle == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text! += numTitle
+            }
+            
+        }
+        
+    }
+    
 }
 
